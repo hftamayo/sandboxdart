47d0cea3696f362d5d8c4de0aa696e5239d643da
hftamayo <hftamayo@gmail.com>
2023-01-12 11:08:40 -0600

    function for employees map iteration

--- focusbcamp/semana1/empleados.dart
+++ focusbcamp/semana1/empleados.dart
@@ -1,21 +1,21 @@
 class Company {
-  final String empName; //no-nullable
-  final double empSalary; //no-nullable
-
-  Company(this.empName, this.empSalary);
-  Company.fromJson(Map<String, dynamic> employees)
-      : empName = employees["name"],
-        empSalary = employees["salary"];
-
-  double averageSalary() {
-    double averageSal = employees.map((e) => e['salary']!).average;
-    return averageSal;
+  final Map employees;
+  
+  Company({this.employees = const {}})
+   
+  
+   void viewEmployees() {
+    this.employees.forEach((k, v) => print('Name : ${k}, Salary: ${v}'));
   }
+  
 }
 
+
 void main() {
-  final Company myCompany =
-      Company(employees: {"Michael": 2, "Jim": 4, "Pam": 6});
-  final double averageSalary = myCompany.averageSalary();
-  print(averageSalary);
+  final Company myCompany = Company(employees: {
+    "Michael": 2.5,
+    "Jim": 4.75,
+    "Pam": 6.80
+  });
+  myCompany.viewEmployees();
 }
