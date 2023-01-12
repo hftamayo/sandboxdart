073e0de088d9cc945dfbb27850c2e3557b11eee9
hftamayo <hftamayo@gmail.com>
2023-01-12 12:02:27 -0600

    firt version using for loop in the avgSalary

--- focusbcamp/semana1/empleados.dart
+++ focusbcamp/semana1/empleados.dart
@@ -1,21 +1,30 @@
 class Company {
-  final Map employees;
-  
-  Company({this.employees = const {}})
-   
-  
-   void viewEmployees() {
-    this.employees.forEach((k, v) => print('Name : ${k}, Salary: ${v}'));
+  final Map<String, double> employees;
+
+  Company({this.employees = const {}});
+
+  double averageSalary() {
+    double avgSal = 0;
+    List<double> salaries = employees.values.toList();
+
+    for (double salary in salaries) {
+      avgSal += salary;
+    }
+    avgSal = avgSal / employees.length;
+
+    return avgSal;
   }
-  
+
+  void viewEmployees() {
+    employees.forEach((k, v) => print('Name : ${k}, Salary: ${v}'));
+  }
 }
 
-
 void main() {
-  final Company myCompany = Company(employees: {
-    "Michael": 2.5,
-    "Jim": 4.75,
-    "Pam": 6.80
-  });
+  final Company myCompany =
+      Company(employees: {"Michael": 2.5, "Jim": 4.75, "Pam": 6.80});
+
+  final double averageSalary = myCompany.averageSalary();
+  print(averageSalary);
   myCompany.viewEmployees();
 }
